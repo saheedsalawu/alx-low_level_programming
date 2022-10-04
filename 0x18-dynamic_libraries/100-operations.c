11fbafd82b381a3ca9d47395257eeec4137d056c
root <saheedsalawu121@gmail.com>
2022-10-03 21:21:56 -0700

    100 operation

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+int add(int a, int b)
+{
+		return (a + b);
+}
+int sub(int a, int b)
+{
+		return (a - b);
+}
+int mul(int a, int b)
+{
+		return (a * b);
+}
+int div(int a, int b)
+{
+		return (a / b);
+}
+int mod(int a, int b)
+{
+		return (a % b);
+}
